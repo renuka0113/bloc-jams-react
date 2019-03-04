fee51565ba1371d0eae602271b9e2362c96ec562
Renuka <renukaalurkar@gmail.com>
2019-03-04 11:35:19 -0600

    Assignment bloc-jams-albumpage completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
@@ -18,12 +19,14 @@ class App extends Component {
           <nav>
             <Link to='/'>Landing</Link>
             <Link to='/library'>Library</Link>
+            <Link to='/album'>Album</Link>
           </nav>
             <h1>Bloc Jams</h1>
           </header>
           <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
+          <Route path="/album" component={Album}/>
           </main>
       </div>
     );
