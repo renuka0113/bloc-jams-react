dc4a7f2d907a801871fa6e03447ed97d00896198
Renuka <renukaalurkar@gmail.com>
2019-03-09 22:49:22 -0600

    bloc-jams-react-album checkpoint completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -31,7 +31,17 @@ class Album extends Component {
          <div id="release-info">{this.state.album.releaseInfo}</div>
         </div>
      </section>
-     </section>
+
+     <table id="song-list">
+           <colgroup>
+             <col id="song-number-column" />
+             <col id="song-title-column" />
+             <col id="song-duration-column" />
+           </colgroup>
+           <tbody>
+           </tbody>
+         </table>
+         </section>
 
     );
   }
