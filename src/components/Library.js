66cd3585e4a1250c79da0084d2c3a20cdcb21965
Renuka <renukaalurkar@gmail.com>
2019-03-11 13:39:04 -0500

    removed comments

--- src/components/Library.js
+++ src/components/Library.js
@@ -5,7 +5,7 @@ import React, {Component} from 'react';
 class Library extends Component{
   constructor(props){
     super(props);
-    //in the statement below, we are assigning all of the albumData to albums.
+    
 
     this.state={albums:albumData};
 
@@ -15,8 +15,7 @@ class Library extends Component{
     return(
       <section className='library'>
       {
-      //album is each element of the albums array.index is the index of the array. The albumand index are variables here,if you call them a and b, it would
-      //work just fine.index is not a keyword.
+
       this.state.albums.map( (album, index) =>
        <Link to={`/album/${album.slug}`} key={index}>
         <img src={album.albumCover} alt={album.title} />
