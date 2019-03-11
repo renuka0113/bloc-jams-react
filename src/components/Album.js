3eb5dd52ee88de87e44b6e8e0e9c808d38a901d1
Renuka <renukaalurkar@gmail.com>
2019-03-11 11:19:02 -0500

    assignment album in progress

--- src/components/Album.js
+++ src/components/Album.js
@@ -20,18 +20,21 @@ class Album extends Component {
          };
        }
 
+
+
   render() {
     return (
       <section className="album">
       <section id="album-info">
-        <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-        <div className="album-details">
-        <h1 id="album-title">{this.state.album.title}</h1>
-        <h2 className="artist">{this.state.album.artist}</h2>
-         <div id="release-info">{this.state.album.releaseInfo}</div>
+      <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+      <div className="album-details">
+      <h1 id="album-title">{this.state.album.title}</h1>
+          <h2 className="artist">{this.state.album.artist}</h2>
+          <div id="release-info">{this.state.album.releaseInfo}</div>
+
+
         </div>
      </section>
-
      <table id="song-list">
            <colgroup>
              <col id="song-number-column" />
@@ -39,6 +42,11 @@ class Album extends Component {
              <col id="song-duration-column" />
            </colgroup>
            <tbody>
+           this.state.album.songs((song,index)=>
+           <tr key={index}>
+           {this.album.songs.title}
+           {this.album.songs.duration}
+           </tr>
            </tbody>
          </table>
          </section>
